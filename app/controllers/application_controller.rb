680f9a8131db1335248ffb785a6731bb1951e417
Kentaro2335 <uske211279@gmail.com>
2025-07-11 09:53:38 +0900

    ユーザー編集機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  protected
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
-    end
+  # 編集後のリダイレクト先を指定
+  def after_update_path_for(resource)
+    root_path # たとえばトップページなど、好きな場所に変更可
   end
 
-  # ストロングパラメータの追加
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [
                                         :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date
@@ -18,5 +16,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [
                                         :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date
                                       ])
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Devise modules
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # 以下が必要
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_date, presence: true
 end
