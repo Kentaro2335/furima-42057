816784381f816d965946fc46e425246d9bccf5dc
Kentaro2335 <uske211279@gmail.com>
2025-08-07 15:36:20 +0900

    LGTM後の任意修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,5 @@ class User < ApplicationRecord
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は英字と数字の両方を含めてください' }
 
   has_many :orders
+  has_many :items
 end
